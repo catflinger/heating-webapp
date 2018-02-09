dc14ed599fdf949dbf4b7a91db9d495413c7c3ec
Paul Drury <paul@drurys.org>
2018-02-09 12:45:04 +0000

    Work in progress on progam edit

--- src/app/program-edit/program-edit.component.ts
+++ src/app/program-edit/program-edit.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { FormBuilder, Validators } from '@angular/forms';
+import { FormControl, FormGroup, Validators, FormArray } from '@angular/forms';
 
 import { IProgramService, INJECTABLES } from '../common/injectables';
 import { Program } from '../common/program';
@@ -14,24 +14,36 @@ export class ProgramEditComponent implements OnInit {
     private sub: any;
     private program: Program;
 
-    public loginForm = this.fb.group({
-        name: ["", Validators.required],
-        hwTemp: ["", Validators.required]
-      });
+    private form: FormGroup;
 
-    constructor(public fb: FormBuilder, private route: ActivatedRoute, @Inject(INJECTABLES.ProgramService) private programService: IProgramService) { }
+    constructor(private route: ActivatedRoute, @Inject(INJECTABLES.ProgramService) private programService: IProgramService) { }
 
     ngOnInit() {
+
         this.sub = this.route.params.subscribe(params => {
             let id = params['id']; 
 
             this.programService.getProgram(id).subscribe(
                 (p) => {
                     this.program = p;
+
+                    this.form = new FormGroup ({
+                        id: new FormControl(p.id),
+                        name: new FormControl(p.name,  Validators.required),
+                        hwmax: new FormControl(p.hwmax,  Validators.required),
+                        hwmin: new FormControl(p.hwmin,  Validators.required),
+                        slots: new FormArray([])
+                    });
+
+                    p.slots.forEach((slotValue) => {
+                        this.slotArray.push(new FormControl(slotValue));
+                    });
                 }
             );
         });
     }
+
+    get slotArray(): FormArray { return this.form.get('slots') as FormArray}
 
     ngOnDestroy() {
         if (this.sub) {
@@ -39,3 +51,4 @@ export class ProgramEditComponent implements OnInit {
         }
     }
 }
+
